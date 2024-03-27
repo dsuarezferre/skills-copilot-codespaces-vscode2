754d27c6e83dc438994e970b5c8558a11e9a6deb
dsuarezferre <100166861+dsuarezferre@users.noreply.github.com>
2024-03-27 23:41:17 +0000

    3d commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// Create Web Server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'data/comments.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/api/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
+
+
+
+
